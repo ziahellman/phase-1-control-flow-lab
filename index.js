a697ea5137cbe37acff9aaece47142c84553a726
Zia Hellman <ziahellman@gmail.com>
2022-05-21 14:37:06 +0300

    commit tree c5345aeddbe50711bec90392d8faa39bd255955f

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  let result;
+  if (feet < 400) {
+    result = "This one is on me!";
+  } else if (feet < 2000) {
+    result = "That will be twenty bucks.";
+  } else if (feet < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else {
+    result = "No can do.";
+  }
+
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let result;
+  if (city === "NYC") {
+    result = "Ok, sounds good.";
+  } else if (city === "Pittsburgh") {
+    result = "No go.";
+  }
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let result;
+  if (tip === "generous") {
+    result = "Thank you so much.";
+  } else if (tip === "not as generous") {
+    result = "Thank you.";
+  } else {
+    result = "Bye.";
+  }
+  return result;
 }
